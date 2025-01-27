b9cc96c3f892e8b29824c6949a2883d5cf35071e
MartinJeanne <martinjeanne.dev@gmail.com>
2025-01-27 19:28:02 +0100

    plusMinus chall finished

---
+++ src/plusMinus.ts
@@ -0,0 +1,17 @@
+function plusMinus(arr: number[]): void {
+    let positive = 0;
+    let negative = 0;
+    let zero = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) positive++;
+        else if (arr[i] < 0) negative++;
+        else zero++;
+    }
+    console.log((positive/arr.length).toFixed(6));
+    console.log((negative/arr.length).toFixed(6));
+    console.log((zero/arr.length).toFixed(6));
+}
+
+
+plusMinus([1, 1, 0, -1, -1]);
