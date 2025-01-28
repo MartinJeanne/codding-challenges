ea09fb3b37ea3ee7c9339b47c91ef9e206105d32
MartinJeanne <martinjeanne.dev@gmail.com>
2025-01-28 16:56:50 +0100

    removeElemnt finished

---
+++ leetcode/removeElementTwoPointers.ts
@@ -0,0 +1,13 @@
+function removeElement(nums: number[], val: number): number {
+    let k = 0; // Compteur pour les éléments non égaux à `val`
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i]; // Déplace l'élément non égal à `val` au début
+            k++;
+        }
+    }
+    console.log(nums);
+    return k;
+};
+
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
