e4e5a0514745635e0ff6c61e6d91751816189509
MartinJeanne <martinjeanne.dev@gmail.com>
2025-01-30 11:29:09 +0100

    jumpGame draft

---
+++ leetcode/JumpGame.ts
@@ -0,0 +1,11 @@
+// TODO
+function canJump(nums: number[]): boolean {
+    let toReach = 1;
+    for (let i = nums.length - 2; i >= 0; i--) {
+        
+    }
+    return false;
+};
+
+console.log(canJump([2, 3, 1, 1, 4]));
+console.log(canJump([3, 2, 1, 0, 4]));
