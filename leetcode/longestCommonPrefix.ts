63ceb58f2919f1ab865d4b5d49c1be5dc96cb380
MartinJeanne <martinjeanne.dev@gmail.com>
2025-01-31 13:50:29 +0100

    longestCommonPrefix vertical scanning (solution)

--- leetcode/longestCommonPrefix.ts
+++ leetcode/longestCommonPrefix.ts
@@ -102,9 +102,21 @@ function longestCommonPrefix4(strs: string[]): string {
     return prefix;
 }
 
-console.log(longestCommonPrefix4(["flower", "flow", "flight"]));
-console.log(longestCommonPrefix4(["reflower", "flow", "flight"]));
-console.log(longestCommonPrefix4(["c", "acc", "ccc"]));
 
-let str = 'salut toi';
-console.log(str.slice(0, str.length - 1));
+
+// Vertical scanning
+function longestCommonPrefix5(strs: string[]): string {
+    if (strs == null || strs.length == 0) return "";
+    for (let i = 0; i < strs[0].length; i++) {
+        let c = strs[0][i];
+        for (let j = 1; j < strs.length; j++) {
+            if (i == strs[j].length || strs[j].charAt(i) != c)
+                return strs[0].substring(0, i);
+        }
+    }
+    return strs[0];
+}
+
+//console.log(longestCommonPrefix5(["flower", "flow", "flight"]));
+//console.log(longestCommonPrefix5(["reflower", "flow", "flight"]));
+console.log(longestCommonPrefix5(["c", "acc", "ccc"]));
