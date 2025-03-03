a29e140642492f3ef5eaf4a094f795e3dcef8fcc
MartinJeanne <martinjeanne.dev@gmail.com>
2025-03-03 21:28:55 +0100

    twoSum done

---
+++ leetcode/twoSum.ts
@@ -0,0 +1,7 @@
+function twoSum(nums: number[], target: number): number[] {
+    for (let i = 0; i < nums.length; i++) {
+        for (let x = i + 1; x < nums.length; x++) {
+            if (nums[i] + nums[x] === target) return [i, x];
+        }
+    }
+}
