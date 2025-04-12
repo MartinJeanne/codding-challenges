57549ee16c0e3df8ec5148e2175c5efbc7675534
MartinJeanne <martinjeanne.dev@gmail.com>
2025-04-12 12:30:09 +0200

    haystack done

--- Java/src/main/java/org/example/Haystack.java
+++ Java/src/main/java/org/example/Haystack.java
@@ -5,10 +5,22 @@ import java.util.Map;
 
 public class Haystack {
     public static int strStr(String haystack, String needle) {
-        return 1;
+        if (needle.equals(haystack)) return 0;
+        if (needle.length() >= haystack.length()) return -1;
+
+        for (int i = 0; i < (haystack.length() + 1) - needle.length(); i++) {
+            StringBuilder h = new StringBuilder();
+            for (int j = 0; j < needle.length(); j++) {
+                h.append(haystack.charAt(i + j));
+            }
+            System.out.println(h);
+            if (h.toString().equals(needle)) return i;
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
-        System.out.println(strStr("IV", "e"));
+        System.out.println(strStr("yours", "ou"));
+        System.out.println(strStr("abc", "c"));
     }
 }
